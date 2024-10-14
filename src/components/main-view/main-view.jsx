4267764a783522bf70799b129e903067be2a7b82
GianBo <162351219+Fonchosama@users.noreply.github.com>
2024-10-14 22:40:16 +0200

    changes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,42 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: 'Interstellar',
-      image:
-        'https://m.media-amazon.com/images/I/A1JVqNMI7UL._AC_UF894,1000_QL80_.jpg',
-      director: 'Christopher Nolan',
-      genre: 'Sci-fi',
-      description:
-        'A genre that explores imaginative and futuristic concepts, often involving advanced technology, space exploration, time travel, and extraterrestrial life.',
-    },
-    {
-      id: 2,
-      title: 'Inception',
-      image:
-        'https://m.media-amazon.com/images/I/51oAya-AtgL._SX300_SY300_QL70_ML2_.jpg',
-      director: 'Christopher Nolan',
-      genre: 'Sci-fi',
-      description:
-        'A skilled thief, the absolute best in the dangerous art of extraction, steals secrets from deep within the subconscious during the dream state.',
-    },
-    {
-      id: 3,
-      title: 'The Dark Knight',
-      image:
-        'https://m.media-amazon.com/images/I/81rGCm0PyHL.__AC_SX300_SY300_QL70_ML2_.jpg',
-      director: 'Christopher Nolan',
-      genre: 'Action',
-      description:
-        'Batman sets out to dismantle the remaining criminal organizations that plague Gotham, but the Joker thrusts him into a personal conflict.',
-    },
-  ]);
-
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch('https://gianflix-02d504c4ae81.herokuapp.com/movies')
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.movies.map((movie) => {
+          return {
+            _id: movie._id,
+            Title: movie.title,
+            Description: movie.description,
+            Genre: movie.genre,
+            Director: movie.director,
+          };
+        });
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,4 +9,12 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    directors: PropTypes.string,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };

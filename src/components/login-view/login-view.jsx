0a451761ab87997e3903db0970ae58f089f4b1fa
GianBo <162351219+Fonchosama@users.noreply.github.com>
2024-10-24 00:00:40 +0200

    persisting a login session

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -15,15 +15,32 @@ export const LoginView = ({ onLoggedIn }) => {
 
     fetch('https://gianflix-02d504c4ae81.herokuapp.com/login', {
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify(data),
-    }).then((response) => {
-      if (response.ok) {
-        onLoggedIn(username);
-      } else {
-        alert('Login failed');
-      }
-    });
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Login response: ', data);
+        if (data.user) {
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert('No such user');
+        }
+      })
+      .catch((e) => {
+        alert('Something went wrong');
+      });
   };
+
+  if (data.user) {
+    localStorage.setItem('user', JSON.stringify(data.user));
+    localStorage.setItem('token', data.token);
+    onLoggedIn(data.user, data.token);
+  } else {
+    alert('No such user');
+  }
 
   return (
     <form onSubmit={handleSubmit}>
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,29 +4,53 @@ import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem('user'));
+  const storedToken = localStorage.getItem('token');
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
-  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    fetch('https://gianflix-02d504c4ae81.herokuapp.com/movies')
+    if (!token) {
+      return;
+    }
+
+    fetch('https://myflixmoviedb.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}` },
+    })
       .then((response) => response.json())
       .then((data) => {
-        const moviesFromApi = data?.map((movie) => {
-          return {
-            _id: movie._id,
-            Title: movie.Title,
-            Description: movie.Description,
-            Genre: movie.Genre,
-            Director: movie.Director,
-          };
-        });
-        setMovies(moviesFromApi);
+        console.log(data);
       });
-  }, []);
+  }, [token]);
+
+  // useEffect(() => {
+  //   fetch('https://gianflix-02d504c4ae81.herokuapp.com/movies')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const moviesFromApi = data?.map((movie) => {
+  //         return {
+  //           _id: movie._id,
+  //           Title: movie.Title,
+  //           Description: movie.Description,
+  //           Genre: movie.Genre,
+  //           Director: movie.Director,
+  //         };
+  //       });
+  //       setMovies(moviesFromApi);
+  //     });
+  // }, []);
 
   if (!user) {
-    return <LoginView onLoggedIn={(user) => setUser(user)} />;
+    return (
+      <LoginView
+        onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }}
+      />
+    );
   }
 
   if (selectedMovie) {
@@ -53,6 +77,23 @@ export const MainView = () => {
           }}
         />
       ))}
+      <button
+        onClick={() => {
+          setUser(null);
+        }}
+      >
+        Logout
+      </button>
     </div>
   );
 };
+
+<button
+  onClick={() => {
+    setUser(null);
+    setToken(null);
+    localStorage.clear();
+  }}
+>
+  Logout
+</button>;

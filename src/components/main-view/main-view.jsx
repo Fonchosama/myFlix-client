12dd2df0e2788ab62faf2a7833a2374fd701f097
GianBo <162351219+Fonchosama@users.noreply.github.com>
2024-10-30 23:58:56 +0100

    last changes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,8 @@ import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
 import { SignupView } from '../signup-view/signup-view';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem('user'));
@@ -55,33 +57,59 @@ export const MainView = () => {
   }
 
   return (
-    <div>
+    <Row className="justify-content-md-center">
       {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
-          }}
-        />
+        <Col className="mb-3" key={movie.id} md={3}>
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+            onMovieClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        </Col>
       ))}
       <button
         onClick={() => {
           setUser(null);
+          setToken(null);
+          localStorage.clear();
         }}
       >
         Logout
       </button>
-    </div>
+    </Row>
   );
 };
 
-<button
-  onClick={() => {
-    setUser(null);
-    setToken(null);
-    localStorage.clear();
-  }}
->
-  Logout
-</button>;
+//   return (
+//     <div>
+//       {movies.map((movie) => (
+//         <MovieCard
+//           key={movie.id}
+//           movie={movie}
+//           onMovieClick={(newSelectedMovie) => {
+//             setSelectedMovie(newSelectedMovie);
+//           }}
+//         />
+//       ))}
+//       <button
+//         onClick={() => {
+//           setUser(null);
+//         }}
+//       >
+//         Logout
+//       </button>
+//     </div>
+//   );
+// };
+
+// <button
+//   onClick={() => {
+//     setUser(null);
+//     setToken(null);
+//     localStorage.clear();
+//   }}
+// >
+//   Logout
+// </button>;

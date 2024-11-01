7536c9c199285b7b6abac1e07d274705b4f36118
GianBo <162351219+Fonchosama@users.noreply.github.com>
2024-11-01 23:54:59 +0100

    a

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,8 +3,8 @@ import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
 import { SignupView } from '../signup-view/signup-view';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Row, Col, Button } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem('user'));

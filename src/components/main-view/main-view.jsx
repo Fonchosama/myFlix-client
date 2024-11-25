bf6ec6265da5d2f620f93b03fac2dee53db22798
GianBo <162351219+Fonchosama@users.noreply.github.com>
2024-11-25 11:59:09 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -104,9 +104,7 @@ export const MainView = () => {
                 <Row className="justify-content-md-center">
                   <Col md={5}>
                     <LoginView
-                      onLoggedIn={
-                        ((user, token) => setUser(user), setToken(token))
-                      }
+                      onLoggedIn={(user, token) => setUser(user); setToken(token)}
                     />
                   </Col>
                 </Row>
